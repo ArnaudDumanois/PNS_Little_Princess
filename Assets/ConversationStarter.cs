46bc50db6ca617413daf40c3d6516cfa471680e1
amandinemart1 <amandine-martin@sfr.fr>
2024-04-09 13:35:35 +0200

    dialogue canard fonctionne git add .!

--- Assets/ConversationStarter.cs
+++ Assets/ConversationStarter.cs
@@ -5,18 +5,42 @@ using DialogueEditor;
 
 public class ConversationStarter : MonoBehaviour
 {
-
     [SerializeField] private NPCConversation myConversation;
+
+    private bool conversationStarted = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !conversationStarted)
         {
-            Debug.Log(Input.GetKeyDown(KeyCode.X));
-            if(Input.GetKeyDown(KeyCode.X)){
-                Debug.Log("D key pressed");
+            Debug.Log("Player entered trigger zone.");
+            conversationStarted = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && conversationStarted)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log("X key pressed");
                 ConversationManager.Instance.StartConversation(myConversation);
+                conversationStarted = false; // Resetting for potential future conversations
+            }
+            else
+            {
+                Debug.Log("X key not pressed");
             }
         }
-    }      
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited trigger zone.");
+            conversationStarted = false; // Resetting if player exits before pressing X
+        }
+    }
 }
